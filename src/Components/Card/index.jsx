6dd16af248940432a5cb8a59b74ca92d6f66f619
vitor Higor <carlosviniciussampaio@hotmail.com>
2023-11-23 22:42:18 -0300

    criando componente card

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const ContainerStyled = styled.div`
+
+`
+
+function Card() {
+
+
+
+    return (
+        <ContainerStyled>
+
+            
+        </ContainerStyled>
+    )
+}
+
+export default Card;
