cc30d8878fea567b84f90b502650dc6edf6f030c
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-11-24 00:29:04 -0300

    aprimorando os estilos dos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1>Pokedex</h1>
+      <Card />
       </header>
     </div>
   );
--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,17 +1,46 @@
 import styled from 'styled-components'
+import pokemons from "./pokemon.json"
 
 const ContainerStyled = styled.div`
+    font-family: 'Roboto', sans-serif;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+   
+`
+const CardStyled = styled.section`
+    background-color: #14A06F;
+    width: 90%;
+    border-radius: 20px;
+    margin: .3em;
+    padding: 1.5em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    font-size: 12px;
+    color: #fff;
+    height: 130px;
+`
 
+const ImgPokemonStyled = styled.img`
+    max-width: 100%;
+    height: 70px;
+    align-self: flex-end;
 `
 
 function Card() {
-
-
-
     return (
         <ContainerStyled>
-
-            
+            {
+                pokemons.map((pokemon) => (
+                    <CardStyled>
+                        <h1> {pokemon.nome} </h1>
+                        <ImgPokemonStyled src={pokemon.img} alt={pokemon.nome} ></ImgPokemonStyled>
+                    </CardStyled>
+                ))
+            }
+            {/* <h1> {pokemons.nome} </h1>
+            <img src={pokemons.img} alt={pokemons.nome} />  */}
         </ContainerStyled>
     )
 }
