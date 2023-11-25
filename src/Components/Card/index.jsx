e05a42f3bc2049a3836122cf3a0132a7c8fa3827
carlos vinicius <carlosviniciussampaio@hotmail.com>
2023-11-24 23:39:46 -0300

    estilos dos cards mobile

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -6,26 +6,49 @@ const ContainerStyled = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr;
    
+    h1 {
+        margin-bottom: 5px;
+        margin-top: 50px;
+    }
 `
 const CardStyled = styled.section`
     background-color: #14A06F;
-    width: 90%;
+    width: 80%;
     border-radius: 20px;
-    margin: .3em;
-    padding: 1.5em;
+    margin: 0.3em;
+    padding: 1.2em;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     align-items: flex-start;
     font-size: 12px;
     color: #fff;
-    height: 130px;
+    height: 135px;
+    box-sizing: content-box;
 `
 
 const ImgPokemonStyled = styled.img`
     max-width: 100%;
     height: 70px;
     align-self: flex-end;
+    position: relative;
+    top: -30px;
+`
+const Type = styled.ol`
+    list-style: none;
+    display: flex;
+    gap: 2px;
+    flex-direction: column;
+    padding: 0;
+    margin: 0;
+    
+
+    li {
+        background-color: gray;
+        border-radius: 20px;
+        padding: 8px 15px 8px 15px;
+        text-align: center;
+    }
 `
 
 function Card() {
@@ -33,8 +56,21 @@ function Card() {
         <ContainerStyled>
             {
                 pokemons.map((pokemon) => (
-                    <CardStyled>
-                        <h1> {pokemon.nome} </h1>
+                    <CardStyled key={pokemon.id}>
+                        
+                        <h1> {pokemon.nome}  </h1>
+                        
+                        <Type>
+                            <li>
+                                {pokemon.type}
+                            </li>
+                            {
+                                pokemon.type2 &&   <li>
+                                {pokemon.type2}
+                            </li>
+                            }
+                          
+                        </Type>
                         <ImgPokemonStyled src={pokemon.img} alt={pokemon.nome} ></ImgPokemonStyled>
                     </CardStyled>
                 ))
